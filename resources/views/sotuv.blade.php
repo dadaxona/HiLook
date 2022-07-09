d0f25ac0e37051dd52c2f443006beafa17325f4e
dadaxona <dadaxona7@gmail.com>
2022-07-09 20:03:40 +0500

    s

--- resources/views/sotuv.blade.php
+++ resources/views/sotuv.blade.php
@@ -75,7 +75,7 @@
                 <button onclick="yangilash()" class="btn btn-warning m-0 p-2" style="color: black;">
                     <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
                         <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
-                      </svg>
+                    </svg>
                 </button> 
                 <button onclick="udalit()" id="udalit" class="btn btn-danger m-0 p-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
